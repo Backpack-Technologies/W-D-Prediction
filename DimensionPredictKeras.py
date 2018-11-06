3c594e82efbc2678ada836640d4256727ca2cbe3
Shubhashis Roy <iamdipta@gmail.com>
2018-11-06 23:08:39 +0600

    new update with early stopping

--- DimensionPredictKeras.py
+++ DimensionPredictKeras.py
@@ -4,51 +4,62 @@ import DataParser
 import ProductInfo
 import numpy as np
 
-EPCHOES = 444
+EPCHOES = 4
 NUM_OF_COLS = 300
-MODEL_FILE_NAME = '/Users/dipta007/my-world/backpack/work/DimensionPredict/model/test1/'
+MODEL_FILE_NAME = './modelDP/Weights-best1.h5'
 EMBEDDINGS_FILE = 'data/p2v-embeddings1000000'
 LOAD_DATA_FROM_FILE = True
 
 
-def neural_model():
+def create_neural_model():
     model = keras.Sequential()
 
+    activation_fucntion = keras.layers.PReLU();
+
     # Input Layer
-    model.add(keras.layers.Dense(405, kernel_initializer='normal', input_dim=NUM_OF_COLS, activation="relu",
+    model.add(keras.layers.Dense(405, kernel_initializer='normal', input_dim=NUM_OF_COLS,
                                  kernel_regularizer=keras.regularizers.l2(0.01),
-                                 bias_regularizer=keras.regularizers.l2(0.01)))
-    # model.add(keras.layers.Dropout(0.2, input_shape=(NUM_OF_COLS, )))
+                                 bias_regularizer=keras.regularizers.l2(0.01), name="input"))
+    model.add(activation_fucntion)
 
     # Hidden Layer
-    model.add(keras.layers.Dense(405, kernel_initializer='normal', activation='relu'))
-    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.Dense(405, kernel_initializer='normal', name="hidden1"))
+    model.add(activation_fucntion)
+    model.add(keras.layers.Dropout(0.2))
 
-    # model.add(keras.layers.Dense(164, kernel_initializer='normal', activation='relu'))
-    # model.add(keras.layers.Dropout(0.5))
-
-    # model.add(keras.layers.Dense(2560, kernel_initializer='normal', activation='relu',
-    #                              kernel_regularizer=keras.regularizers.l2(0.01),
-    #                              bias_regularizer=keras.regularizers.l2(0.01)))
-    # model.add(keras.layers.Dropout(0.5))
-    #
-    # model.add(keras.layers.Dense(2560, kernel_initializer='normal', activation='relu',
-    #                              kernel_regularizer=keras.regularizers.l2(0.01),
-    #                              bias_regularizer=keras.regularizers.l2(0.01)))
-    # model.add(keras.layers.Dropout(0.5))
-    #
-    # model.add(keras.layers.Dense(2560, kernel_initializer='normal', activation='relu',
-    #                              kernel_regularizer=keras.regularizers.l2(0.01),
-    #                              bias_regularizer=keras.regularizers.l2(0.01)))
-    # model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.Dense(405, kernel_initializer='normal', name="hidden2"))
+    model.add(activation_fucntion)
+    model.add(keras.layers.Dropout(0.2))
 
     # Output Layer
-    model.add(keras.layers.Dense(4, kernel_initializer='normal', activation='relu'))
+    model.add(keras.layers.Dense(4, kernel_initializer='normal', name="output"))
 
-    optimizer = keras.optimizers.SGD(lr=0.01, clipvalue=0.5)
-    model.compile(loss='mean_absolute_error', optimizer="RMSprop", metrics=['mean_absolute_error'])
-
+    model.compile(loss='mean_absolute_error', optimizer="Adam", metrics=['mean_absolute_error'])
     return model
+
+
+def train_neural_model(model):
+    x_train, x_test, y_train, y_test = DataParser.get_splitted_data_for_model(LOAD_DATA_FROM_FILE)
+
+    print(x_train.shape, y_train.shape)
+
+    # checkpoint_name = './modelDP/Weights-{epoch:03d}--{val_loss:.5f}.hdf5'
+    # checkpoint_name = './modelDP/Weights-best1.hdf5'
+
+    checkpoint = keras.callbacks.ModelCheckpoint(MODEL_FILE_NAME, monitor='val_loss', verbose=1, save_best_only=True,
+                                                 mode='auto')
+
+    earlyStopping = keras.callbacks.EarlyStopping(patience=16, mode="auto")
+    tensorBoard = keras.callbacks.TensorBoard(log_dir="./logs")
+
+    callbacks_list = [checkpoint, earlyStopping]
+
+    # model = create_neural_model()
+    model.fit(x_train, y_train, epochs=EPCHOES, batch_size=444, validation_split=0.2, callbacks=callbacks_list)
+    print("Current one: ", model.evaluate(x_test, y_test, batch_size=44))
+
+    model.load_weights(MODEL_FILE_NAME)
+    print("Best one: ", model.evaluate(x_test, y_test, batch_size=44))
 
 
 def _start_shell(local_ns=None):
@@ -75,19 +86,8 @@ def predict(ind):
 
 
 def main(_):
-    x_train, x_test, y_train, y_test = DataParser.get_splitted_data_for_model(LOAD_DATA_FROM_FILE)
-
-    print(x_train.shape, y_train.shape)
-
-    checkpoint_name = 'Weights-{epoch:03d}--{val_loss:.5f}.hdf5'
-
-    checkpoint = keras.callbacks.ModelCheckpoint(checkpoint_name, monitor='val_loss', verbose=1, save_best_only=True,
-                                                 mode='auto')
-    callbacks_list = [checkpoint]
-
-    model = neural_model()
-    model.fit(x_train, y_train, epochs=EPCHOES, batch_size=444, validation_split=0.2)
-    print(model.evaluate(x_test, y_test, batch_size=44))
+    model = create_neural_model()
+    train_neural_model(model)
 
     _start_shell(locals())
 
